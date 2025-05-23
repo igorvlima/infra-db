061980d9ab048b5cc27779b0cd90f1271488e112
igorvlima <igorveraslima3@gmail.com>
2025-05-23 01:52:29 -0300

    fix db instance class

--- main.tf
+++ main.tf
@@ -38,10 +38,11 @@ resource "aws_db_instance" "db-rds-fiaptech" {
   storage_type            = "gp2"
   engine                  = "postgres"
   engine_version          = "17"
-  instance_class          = "db.t2.micro"
+  instance_class          = "db.t3.micro"
   username                = "pgadmin"
   password                = var.db_password
   publicly_accessible     = false
+  skip_final_snapshot     =  true
   vpc_security_group_ids  = [aws_security_group.sg-rds-fiaptech.id]
 
 lifecycle {
