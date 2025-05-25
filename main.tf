6da64d21c05b4d1c7bd1e37ac018b46c3d1a1928
igorvlima <65581463+igorvlima@users.noreply.github.com>
2025-05-25 15:10:03 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_db_instance" "db-rds-fiaptech" {
   engine                  = "postgres"
   engine_version          = "17"
   instance_class          = "db.t3.micro"
-  username                = var.db_username
+  username                = var.db_user
   password                = var.db_password
   publicly_accessible     = false
   skip_final_snapshot     =  true
